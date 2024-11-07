e90fec36e9827e1d0817897e6da0749c6446f42a
julescavanier <cavanierju@gmail.com>
2024-11-07 14:29:23 +0100

    route to link with db

---
+++ app/Http/Controllers/PingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PingController extends Controller
+{
+    public function ping() {
+        return response()->json(['message' => 'pong'], 200);
+    }
+}
