d6d621b4659b7827c8b2042b1917905bbaba11a3
julescavanier <cavanierju@gmail.com>
2024-11-25 16:22:21 +0100

    readme & timestamp

--- apiAirlux/app/Http/Controllers/PingController.php
+++ apiAirlux/app/Http/Controllers/PingController.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\Controller;
 class PingController extends Controller
 {
     public function ping() {
-        return response()->json(['message' => 'pong'], 200);
+        return response()->json(['timestamp' => time()], 200);
     }
 }
